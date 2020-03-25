b0aacc728abb1f3191259046fcd71bacc797f804
rana <ranasobeid95@gmail.com>
2020-03-25 16:48:54 +0200

    create react app for project delete all unnecessary files install initial packages for client side setup Eslint Configuration for client side #4

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CA wiki website</h1>
+    </div>
+  );
+}
+
+export default App;
