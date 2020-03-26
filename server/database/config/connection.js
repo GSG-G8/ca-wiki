85934e885e4abff00167fa83ca830dea36bed648
rehabas <reremo45@gmail.com>
2020-03-26 21:42:39 +0200

    configure the connection file relates #6

---
+++ server/database/config/connection.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+let dbUrl;
+
+if (process.env.NODE_ENV === 'development') {
+  dbUrl = process.env.DB_URL;
+} else if (process.env.NODE_ENV === 'production') {
+  dbUrl = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === 'test') {
+  dbUrl = process.env.TEST_DB_URL;
+}
+
+const options = {
+  connectionString: dbUrl,
+  ssl: true,
+};
+
+module.exports = new Pool(options);
