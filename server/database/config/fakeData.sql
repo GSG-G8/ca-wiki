4b16b96d7dd641f840be96277fe0426348c0a282
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-03-27 21:18:08 +0200

    insert another data and make some edits > relates #31

--- server/database/config/fakeData.sql
+++ server/database/config/fakeData.sql
@@ -1,10 +1,16 @@
-INSERT INTO admin (username , password) VALUES ('Rana' , '123456');
+INSERT INTO admin (username , password) VALUES ('Mohammed' , '123456');
 
-INSERT INTO cohort (name , githubLink) VALUES 
-('G8' , 'https://github.com/GSG-G8');
+INSERT INTO cohort (name , description , img_url , github_link) VALUES 
+('G8','GazaSkyGeeks Code Academy, 8th Cohort','https://avatars0.githubusercontent.com/u/59821022?s=200&v=4','https://github.com/GSG-G8');
+('G7','GazaSkyGeeks Code Academy, 7th Cohort','https://avatars3.githubusercontent.com/u/52123464?s=200&v=4','https://github.com/GSG-G7');
 
-INSERT INTO student (name , githubLink) VALUES
-('Alaa' , 'https://github.com/AlaaSaadeddin');
+INSERT INTO student (name , img_url , github_link) VALUES
+('Alaa','https://avatars2.githubusercontent.com/u/26024288?s=60&u=573706aef193cc5bcb8a28969cff88bf282bb6ef&v=4','https://github.com/AlaaSaadeddin');
+('Rana','https://avatars1.githubusercontent.com/u/25321550?s=96&v=4','https://github.com/ranasobeid95')
 
-INSERT INTO project (name , imgUrl) VALUES 
-('ca-wiki' , 'https://lh3.googleusercontent.com/proxy/fp_bF_rbMIKyCfgdgWodyuM9LGt3HwGgM8AMnQ4qxjftKcvEdmhngdaeA8F6xFgRDHVzezPLT6YZarpBcqnMD5WtAvUhKJXcWS7qvS6Bn3CllitLttt_uA');
+INSERT INTO project (name , description , img_url , github_link , website_link , project_type) VALUES 
+('ca-wiki',
+'Ca-wiki is a web application which allows clients to view all cohorts that have been enrolled in Code Academy. Clients can view all students who graduated from the academy so that they can view every student and his/her projects he/she participated in, his/her github page',
+'https://lh3.googleusercontent.com/proxy/fp_bF_rbMIKyCfgdgWodyuM9LGt3HwGgM8AMnQ4qxjftKcvEdmhngdaeA8F6xFgRDHVzezPLT6YZarpBcqnMD5WtAvUhKJXcWS7qvS6Bn3CllitLttt_uA',
+'https://github.com/GSG-G8/ca-wiki/tree/ed9f4cd9b5dc428f5420fe9a880a27e63f5f04d3',
+'https://github.com/GSG-G8/ca-wiki/blob/ed9f4cd9b5dc428f5420fe9a880a27e63f5f04d3/%5Blink%5D','locally project');
