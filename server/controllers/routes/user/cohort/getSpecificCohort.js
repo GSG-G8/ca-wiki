cbcea2595c736b171c9a81109ba4d3780cfbc0ae
mu7ammadabed <mu7ammadabed@gmail.com>
2020-03-30 00:44:33 +0300

    Handle nonexisting cohorts in addition to add test for it with destructure consts

--- server/controllers/routes/user/cohort/getSpecificCohort.js
+++ server/controllers/routes/user/cohort/getSpecificCohort.js
@@ -7,10 +7,13 @@ exports.getSpecificCohort = async (req, res, next) => {
     const { cohortid } = req.params;
     const { rows } = await getSpecificCohort(cohortid);
     const data = { ...rows[0] };
-    if(data.id) {
+    if (data.id) {
       res.json({ statusCode: 200, data });
     } else {
-      res.json({ statusCode: 200, message: "Sorry There's no cohort for this id" });
+      res.json({
+        statusCode: 200,
+        message: "Sorry There's no cohort for this id",
+      });
     }
   } catch (err) {
     next(err);
