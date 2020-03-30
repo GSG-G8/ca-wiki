fa8c6f0c47c4aeae5df2256a9b688f3a4af40192
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-30 12:31:55 +0300

    convert promise to async/await in deleteCohort.js relates #29

--- server/controllers/routes/admin/cohort/deleteCohort.js
+++ server/controllers/routes/admin/cohort/deleteCohort.js
@@ -1,14 +1,15 @@
 const deleteCohort = require('../../../../database/queries/cohort/deleteCohort');
 
-const cohortDelete = (req, res, next) => {
-  deleteCohort(req.params.cohortId)
-    .then(() =>
-      res.json({
-        StatusCode: 200,
-        data: { message: 'Cohort deleted successfully' },
-      }),
-    )
-    .catch(next);
+const cohortDelete = async (req, res, next) => {
+  try {
+    await deleteCohort(req.params.cohortId);
+    res.json({
+      StatusCode: 200,
+      data: { message: 'Cohort deleted successfully' },
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
