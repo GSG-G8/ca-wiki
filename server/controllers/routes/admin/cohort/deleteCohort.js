c613d5b35cd639169d987985138e4ea30a3b38f2
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-30 20:37:37 +0300

    check if cohort id exist or not relates #29

--- server/controllers/routes/admin/cohort/deleteCohort.js
+++ server/controllers/routes/admin/cohort/deleteCohort.js
@@ -2,11 +2,17 @@ const deleteCohort = require('../../../../database/queries/cohort/deleteCohort')
 
 const cohortDelete = async (req, res, next) => {
   try {
-    await deleteCohort(req.params.cohortId);
-    res.json({
-      StatusCode: 200,
-      data: { message: 'Cohort deleted successfully' },
-    });
+    const check = await deleteCohort(req.params.cohortId);
+    if (check.rowCount !== 0) {
+      res.json({
+        StatusCode: 200,
+        data: { message: 'Cohort deleted successfully' },
+      });
+    } else {
+      const err = new Error();
+      err.message = 'cohort id does not exist';
+      next(err);
+    }
   } catch (err) {
     next(err);
   }
