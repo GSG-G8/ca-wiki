f9ff73003025db7bd1d0c4705110098cc3efe2b3
mu7ammadabed <mu7ammadabed@gmail.com>
2020-03-31 02:13:26 +0300

    define sent data as a const

--- test/index.test.js
+++ test/index.test.js
@@ -13,15 +13,17 @@ afterAll(() => {
 });
 
 describe('Cohort', () => {
+  const data = {
+    name: 'G1',
+    description: 'Code GazaSkyGeeksAcademy, 1st Cohort',
+    img_url: 'https://avatars0.githubusercontent.com/u/59821022?s=200&v=4',
+    github_link: 'https://github.com/GSG-G1',
+  };
+
   test('PUT Route /cohort/1 status 200, json header, send data ', (done) => {
     return request(app)
       .put('/api/v1/cohort/1')
-      .send({
-        name: 'G1',
-        description: 'Code GazaSkyGeeksAcademy, 1st Cohort',
-        img_url: 'https://avatars0.githubusercontent.com/u/59821022?s=200&v=4',
-        github_link: 'https://github.com/GSG-G1',
-      })
+      .send(data)
       .expect(200)
       .expect('Content-Type', /json/)
       .end(async (err, res) => {
@@ -47,12 +49,7 @@ describe('Cohort', () => {
   test('PUT Route /cohort/4 status 404, json header, send data ', (done) => {
     return request(app)
       .put('/api/v1/cohort/4')
-      .send({
-        name: 'G4',
-        description: 'Code GazaSkyGeeksAcademy, 4th Cohort',
-        img_url: 'https://avatars0.githubusercontent.com/u/59821022?s=200&v=4',
-        github_link: 'https://github.com/GSG-G4',
-      })
+      .send(data)
       .expect(404)
       .expect('Content-Type', /json/)
       .end(async (err, res) => {
