9bbdfdf91acffafe6a1ccd2fff5df73868872d21
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-30 11:42:34 +0300

    make test of admin delete route /cohort/:cohortId relates #29

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,28 @@
-test('Initial test', () => {
-  expect(1).toBe(1);
+const request = require('supertest');
+const connection = require('../server/database/config/connection');
+const dbBuild = require('../server/database/config/build');
+
+const app = require('../server/app');
+
+beforeAll(() => {
+  return dbBuild();
+});
+
+afterAll(() => {
+  return connection.end();
+});
+
+describe('Admin, (/cohorts/:cohortId)', () => {
+  test('Route /cohorts/1 status 200, json header, data.message = Cohort deleted successfully ', (done) => {
+    return request(app)
+      .delete('/api/v1/cohorts/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const { data } = res.body;
+        if (err) return done(err);
+        expect(data.message).toBe('Cohort deleted successfully');
+        done();
+      });
+  });
 });
