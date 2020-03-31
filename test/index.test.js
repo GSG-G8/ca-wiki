a546d2c440684cc61713f28384d4ee91e34a5a1f
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-03-31 12:37:15 +0300

    make a test for Route get /cohorts relates #14

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,30 @@
-test('Initial test', () => {
-  expect(1).toBe(1);
+// /* eslint-disable jest/expect-expect */
+
+const request = require('supertest');
+const connection = require('../server/database/config/connection');
+const dbBuild = require('../server/database/config/build');
+
+const app = require('../server/app');
+
+beforeAll(() => {
+  return dbBuild();
+});
+
+afterAll(() => {
+  return connection.end();
+});
+
+describe('Cohort', () => {
+  test('Route /cohorts status 200, json header, data.id = data.id', (done) => {
+    return request(app)
+      .get('/api/v1/cohorts')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { data } = res.body;
+        expect(data.id).toBe(data.id);
+        done();
+      });
+  });
 });
