11e48882e5458b10610b47d8979a679c6fb07e92
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-31 15:28:22 +0300

    modify addProjectQuerey argument relates #24

--- server/controllers/routes/admin/project/addProject.js
+++ server/controllers/routes/admin/project/addProject.js
@@ -3,25 +3,8 @@ const { addProjectSchema } = require('../../../../validation');
 
 const addProject = async (req, res, next) => {
   try {
-    const {
-      name,
-      description,
-      imgUrl,
-      githubLink,
-      websiteLink,
-      projectType,
-      cohortId,
-    } = req.body;
     await addProjectSchema.validate(req.body);
-    await addProjectQuery(
-      name,
-      description,
-      imgUrl,
-      githubLink,
-      websiteLink,
-      projectType,
-      cohortId,
-    );
+    await addProjectQuery(req.body);
     res.json({
       StatusCode: 200,
       data: { message: 'Cohort Added successfully' },
--- server/database/queries/project/addProject.js
+++ server/database/queries/project/addProject.js
@@ -1,17 +1,19 @@
 const connection = require('../../config/connection');
 
-const addProjectQuery = (
-  name,
-  description,
-  imgUrl,
-  githubLink,
-  websiteLink,
-  projectType,
-  cohortId,
-) =>
+const addProjectQuery = (data) => {
+  const {
+    name,
+    description,
+    imgUrl,
+    githubLink,
+    websiteLink,
+    projectType,
+    cohortId,
+  } = data;
   connection.query(
     'INSERT INTO project (name, description, img_url, github_link, website_link, project_type, cohort_id) VALUES ($1, $2, $3, $4, $5, $6, $7)',
     [name, description, imgUrl, githubLink, websiteLink, projectType, cohortId],
   );
+};
 
 module.exports = addProjectQuery;
