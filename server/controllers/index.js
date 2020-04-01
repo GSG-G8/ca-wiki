4d9a85d2e9dc5d26c87fe7cc7b1159c4622c2377
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-04-01 13:08:35 +0300

    fix error handle relates #52

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const admin = require('./routes/admin');
 
-const { serverError } = require('./middlewares/errorHandle');
-
-router.use(serverError);
-
 router.use(admin);
 
 module.exports = router;
