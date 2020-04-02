963a782b994fef002e5774c5eb4721c6fbaee4c0
rehabas <reremo45@gmail.com>
2020-04-02 20:47:52 +0300

    make test for put student router relates #25

--- test/index.test.js
+++ test/index.test.js
@@ -144,7 +144,6 @@ describe('Admin, (/cohorts/:cohortId)', () => {
   });
 });
 
-
 describe('Admin, Post Project', () => {
   test('Route /projects status 200, json header, data.message = Cohort Added successfully ', (done) => {
     const reqData = {
@@ -259,6 +258,93 @@ describe('Delete specific student by ID', () => {
         const { message } = res.body.data;
         if (err) return done(err);
         expect(message).toBe('You enterd wrong student ID');
+        done();
+      });
+  });
+});
+
+describe('Admin, Put project', () => {
+  const validData = {
+    name: 'Rehab',
+    email: 'rehab@gmail.com',
+    imgUrl: 'https://avatars3.githubusercontent.com/u/49806841?s=460&v=4',
+    githubLink: 'https://github.com/rehabas',
+    cohortId: 1,
+  };
+  const invalidData = {
+    name: 'Rehab',
+    email: 'email',
+    imgUrl: 'img url',
+    githubLink: 'github link',
+    cohortId: 1,
+  };
+
+  test('PUT Route /alumni/1 status 200, json header, put data ', (done) => {
+    request(app)
+      .put('/api/v1/alumni/1')
+      .send(validData)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(async (err, res) => {
+        if (err) return done(err);
+        const {
+          data: { message },
+        } = res.body;
+        const { rows } = await connection.query(
+          'SELECT * from student WHERE id = 1',
+        );
+        expect(rows).toHaveLength(1);
+        expect(rows[0]).toEqual({
+          id: 1,
+          name: 'Rehab',
+          email: 'rehab@gmail.com',
+          img_url:
+            'https://avatars3.githubusercontent.com/u/49806841?s=460&v=4',
+          github_link: 'https://github.com/rehabas',
+          cohort_id: 1,
+        });
+        expect(message).toBe("Student's data updated successfully");
+        done();
+      });
+  });
+
+  test('PUT Route /alumni/11 status 404, json header, put data ', (done) => {
+    request(app)
+      .put('/api/v1/alumni/11')
+      .send(validData)
+      .expect(404)
+      .expect('Content-Type', /json/)
+      .end(async (err, res) => {
+        if (err) return done(err);
+        const {
+          data: { message },
+        } = res.body;
+        const { rows } = await connection.query(
+          'SELECT * from student WHERE id = 11',
+        );
+        expect(rows).toHaveLength(0);
+        expect(message).toBe('There is no student for this id');
+        done();
+      });
+  });
+
+  test('PUT Route /alumni/1 status 400, json header, put invalid data ', (done) => {
+    request(app)
+      .put('/api/v1/alumni/1')
+      .send(invalidData)
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end(async (err, res) => {
+        if (err) return done(err);
+        const {
+          data: { message },
+        } = res.body;
+        await connection.query('SELECT * from student WHERE id = 1');
+        expect(message).toEqual([
+          'email must be a valid email',
+          'imgUrl must be a valid URL',
+          'githubLink must be a valid URL',
+        ]);
         done();
       });
   });
