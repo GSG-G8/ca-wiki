3c8b3dfab0db976496e5f9650b71506db41c01eb
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-04-03 17:22:35 +0300

    handle error relates #26

--- server/controllers/routes/user/student/getAlumni.js
+++ server/controllers/routes/user/student/getAlumni.js
@@ -3,7 +3,11 @@ const { getAlumniQuery } = require('../../../../database/queries');
 const getAlumni = async (req, res, next) => {
   try {
     const { rows } = await getAlumniQuery();
-    res.json({ statusCode: 200, data: rows });
+    if (rows.length > 0) {
+      res.json({ statusCode: 200, data: rows });
+    } else {
+      res.json({ statusCode: 200, message: 'No Data' });
+    }
   } catch (err) {
     next(err);
   }
