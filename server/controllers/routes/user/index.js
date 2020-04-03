73a3f0aec6da31787e7e4990508bc5a9ea7139e7
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-04-03 17:02:47 +0300

    remove route

--- server/controllers/routes/user/index.js
+++ server/controllers/routes/user/index.js
@@ -5,7 +5,6 @@ const { getAlumniCohort } = require('./student');
 
 router.get('/cohorts/:cohortId', getSpecificCohort);
 router.get('/cohorts', getCohortsData);
-router.get('/cohorts/:cohortid', getSpecificCohort);
 router.get('/alumni/:cohortId', getAlumniCohort);
 
 module.exports = router;
