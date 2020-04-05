5a50a7ec56248daedb51268c6cf5b3a9636895e8
mu7ammadabed <mu7ammadabed@gmail.com>
2020-04-05 04:59:34 +0300

    Add token and res.cookie

--- server/controllers/routes/admin/login.js
+++ server/controllers/routes/admin/login.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { loginSchema } = require('../../../validation');
 const { loginQuery } = require('../../../database/queries');
 
@@ -6,16 +7,25 @@ const login = async (req, res, next) => {
   try {
     const data = await loginSchema.validate(req.body, { abortEarly: false });
     const userData = await loginQuery(data.username);
-    // console.log(userData);
-    const hashedPasswored = userData.rows[0].password;
-    bcrypt.compare(data.password, hashedPasswored, (err, result) => {
-      if (err) {
-        next(err);
-      } else {
-        // eslint-disable-next-line no-console
-        res.json(result);
-      }
-    });
+    if (userData.rows[0]) {
+      const hashedPasswored = userData.rows[0].password;
+      const userId = userData.rows[0].id;
+      bcrypt.compare(data.password, hashedPasswored, (err, result) => {
+        if (err) {
+          next(err);
+        } else if (result === false) {
+          res
+            .status(400)
+            .json({ statusCode: 400, message: 'Password is incorrect' });
+        } else {
+          const token = jwt.sign({ id: userId }, process.env.SECRET_KEY);
+          res.cookie('token', token);
+          res.json({ msg: 'logged in successfully' });
+        }
+      });
+    } else {
+      res.status(400).json({ statusCode: 400, message: "user doesn't exist" });
+    }
   } catch (err) {
     if (err.errors) {
       res.status(400).json({
