b8593970840071dcba57137747c25e203418300b
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-04-05 10:34:35 +0300

    edit the validation for student schema relates #13

--- server/validation/studentSchema.js
+++ server/validation/studentSchema.js
@@ -5,7 +5,8 @@ const studentSchema = yup.object({
   email: yup.string().email().required(),
   imgUrl: yup.string().url().required(),
   githubLink: yup.string().url().required(),
-  cohortId: yup.number().integer().positive().required(),
+  cohortId: yup.number().positive().required(),
+  studentId: yup.number().positive(),
 });
 
 module.exports = studentSchema;
