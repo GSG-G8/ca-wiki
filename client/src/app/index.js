e8ab805f27fcfd7433750d0ef3b2a76d7a9de86d
rehabas <reremo45@gmail.com>
2020-04-06 23:40:13 +0300

    resolve conflictes relates #77

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <h1>CA wiki website</h1>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
