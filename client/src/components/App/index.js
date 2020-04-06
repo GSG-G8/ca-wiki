19b99a8970110e41f35d6dfb33406859c95e18e2
rehabas <reremo45@gmail.com>
2020-04-06 18:17:42 +0300

    customize antd relates #77

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
 import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>CA wiki website</h1>
+      <Button type="primary">CA wiki website</Button>
     </div>
   );
 }
