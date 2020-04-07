629c8500da05ea9deb4aae40eed497dfded70522
rehabas <reremo45@gmail.com>
2020-04-07 11:22:47 +0300

    add routes relates #77

---
+++ client/src/constants/routes.js
@@ -0,0 +1,8 @@
+export const LOGIN_PAGE = '/login';
+export const HOME_PAGE = '/';
+export const COHORT_PAGE = '/cohorts';
+export const ADD_COHORT = '/add-cohort';
+export const STUDENTS_PAGE = '/students';
+export const ADD_STUDENT = '/add-student';
+export const PROJECTS_PAGE = '/projects';
+export const ADD_PROJECT = '/add-project';
