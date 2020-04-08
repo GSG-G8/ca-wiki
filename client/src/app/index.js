644cb7aa89d6afa6ead7d4c04daca9d9c9e88645
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-04-08 17:57:41 +0300

    remove example relates #82

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'antd/dist/antd.css';
 import './style.css';
-import * as ROUTES from '../constants/routes';
-import AdminHomePage from '../containers/AdminHomePage';
-import AdminCohortPage from '../containers/ AdminCohortPage';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path={ROUTES.HOME_PAGE} exact component={AdminHomePage} />
-          <Route path={ROUTES.COHORT_PAGE} exact component={AdminCohortPage} />
-        </Switch>
-      </Router>
+      <h1>CA wiki website</h1>
     </div>
   );
 }
