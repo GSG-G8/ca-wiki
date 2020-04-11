fe3ea5051ee5a01d859a0d258bcb8bb10018355b
rehabas <reremo45@gmail.com>
2020-04-11 14:14:04 +0300

    conver AdminCard from function component to class component relates #90

--- client/src/components/AdminCard/index.js
+++ client/src/components/AdminCard/index.js
@@ -1,92 +1,116 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
 import './style.css';
 
-const AdminCard = ({
-  imgUrl,
-  name,
-  description,
-  githbUrl,
-  student,
-  websiteLink,
-  studentNames,
-  internalProject,
-  remotelyProject,
-  editCard,
-  deleteCard,
-}) => {
-  return (
-    <div className="admin-card">
-      <div className="name-img-card">
-        <div className="img-card">
-          <img src={imgUrl} alt="card" />
+class AdminCard extends Component {
+  state = {
+    students: [],
+  };
+
+  async componentDidMount() {
+    const { studentNames } = this.props;
+    const result = await axios.get(
+      `../../../api/v1/projects/${studentNames}/alumni`
+    );
+    const { data: students } = result.data;
+    this.setState({ students });
+  }
+
+  render() {
+    const { students } = this.state;
+    const {
+      imgUrl,
+      name,
+      description,
+      githbUrl,
+      student,
+      websiteLink,
+      studentNames,
+      internalProject,
+      remotelyProject,
+      editCard,
+      deleteCard,
+    } = this.props;
+    return (
+      <div className="admin-card">
+        <div className="name-img-card">
+          <div className="img-card">
+            <img src={imgUrl} alt="card" />
+          </div>
+          <div>
+            <h3>Name</h3>
+            <p>{name}</p>
+          </div>
         </div>
+        {description && (
+          <div>
+            <h3>Description</h3>
+            <p>{description}</p>
+          </div>
+        )}
         <div>
-          <h3>Name</h3>
-          <p>{name}</p>
-        </div>
-      </div>
-      {description && (
-        <div>
-          <h3>Description</h3>
-          <p>{description}</p>
-        </div>
-      )}
-      <div>
-        <h3>Github link</h3>
-        <a href={githbUrl} target="blank">
-          Click
-        </a>
-      </div>
-      {student && (
-        <div>
-          <h3>Student</h3>
-          <Link to={`/admin/cohorts/${student}/students`}>View</Link>
-        </div>
-      )}
-      {websiteLink && (
-        <div>
-          <h3>Website</h3>
-          <a href={websiteLink} target="blank">
-            View
+          <h3>Github link</h3>
+          <a href={githbUrl} target="blank">
+            Click
           </a>
         </div>
-      )}
-      {studentNames && (
+        {student && (
+          <div>
+            <h3>Student</h3>
+            <Link to={`/admin/cohorts/${student}/students`}>View</Link>
+          </div>
+        )}
+        {websiteLink && (
+          <div>
+            <h3>Website</h3>
+            <a href={websiteLink} target="blank">
+              View
+            </a>
+          </div>
+        )}
+        {studentNames && (
+          <div>
+            <h3>Student</h3>
+            {students.map((row) => (
+              <p>{row}</p>
+            ))}
+          </div>
+        )}
+        {internalProject && (
+          <div>
+            <h3>Community p</h3>
+            <Link
+              to={`/admin/cohorts/${internalProject}/projects?type=internal`}
+            >
+              View
+            </Link>
+          </div>
+        )}
+        {remotelyProject && (
+          <div>
+            <h3>Clients p</h3>
+            <Link
+              to={`/admin/cohorts/${remotelyProject}/projects?type=remotely`}
+            >
+              View
+            </Link>
+          </div>
+        )}
         <div>
-          <h3>Student</h3>
-          <p>{studentNames}</p>
+          <Button onClick={editCard} className="card-btn edit">
+            Edit
+          </Button>
+          <Button onClick={deleteCard} className="card-btn">
+            Delete
+          </Button>
         </div>
-      )}
-      {internalProject && (
-        <div>
-          <h3>Community p</h3>
-          <Link to={`/admin/cohorts/${internalProject}/projects?type=internal`}>
-            View
-          </Link>
-        </div>
-      )}
-      {remotelyProject && (
-        <div>
-          <h3>Clients p</h3>
-          <Link to={`/admin/cohorts/${remotelyProject}/projects?type=remotely`}>
-            View
-          </Link>
-        </div>
-      )}
-      <div>
-        <Button onClick={editCard} className="card-btn edit">
-          Edit
-        </Button>
-        <Button onClick={deleteCard} className="card-btn">
-          Delete
-        </Button>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 AdminCard.defaultProps = {
   description: undefined,
@@ -104,7 +128,7 @@ AdminCard.propTypes = {
   githbUrl: PropTypes.string.isRequired,
   websiteLink: PropTypes.string,
   student: PropTypes.number,
-  studentNames: PropTypes.string,
+  studentNames: PropTypes.number,
   internalProject: PropTypes.number,
   remotelyProject: PropTypes.number,
   editCard: PropTypes.func.isRequired,
