7b06cf16104f478b0c90941c04596e1fe583e532
rehabas <reremo45@gmail.com>
2020-04-12 19:22:27 +0300

    add route for this component relates #87

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
 import './style.css';
+import AdminProject from '../containers/AdminProjectPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>CA wiki website</h1>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route
+              exact
+              path={ROUTES.COHORT_PROJECTS_PAGE}
+              component={AdminProject}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
