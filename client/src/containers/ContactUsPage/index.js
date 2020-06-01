8fbb226ea121c338af0afc64de94e6cf7e1dd3c0
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-06-01 17:21:45 +0300

    create contact us component relates #114

---
+++ client/src/containers/ContactUsPage/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ContactUS = () => {
+  return (
+    <div>
+      <div>
+        <h3>FOLLOW US</h3>
+        <p>Gaza Sky Geeks</p>
+        <div>FOLLOW</div>
+      </div>
+      <div>
+        <h3>FOLLOW</h3>
+        <p>FOLLOW</p>
+        <p>FOLLOW</p>
+      </div>
+      <div>
+        <h3>FOLLOW</h3>
+        <p>FOLLOW</p>
+        <p>FOLLOW</p>
+        <p>FOLLOW</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUS;
