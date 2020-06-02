608e8346282fe12ce389f2260a0fb99f832a0851
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-06-02 21:46:06 +0300

    create user container class relates #120

---
+++ client/src/components/UserContainer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './style.css';
+import HeaderWithMenu from '../HeaderWithMenu';
+
+class UserContainer extends Component {
+  state = {};
+
+  render() {
+    return <HeaderWithMenu />;
+  }
+}
+
+export default UserContainer;
