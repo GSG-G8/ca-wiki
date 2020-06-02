bae50e340545999ecb4267f3972ee3ad3f902f93
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-06-02 22:11:12 +0300

    add overlay layer beyond the header menu relates #120

--- client/src/components/HeaderWithMenu/index.js
+++ client/src/components/HeaderWithMenu/index.js
@@ -15,76 +15,81 @@ class HeaderWithMenu extends Component {
   render() {
     const { show } = this.state;
     return (
-      <div className="main-header">
-        <div className="header-left">
-          <img src={logo} alt="Code Academy" />
-        </div>
-        <div className="header-right">
-          <SearchOutlined />
-          <MenuOutlined onClick={this.reverseShow} />
-        </div>
-        {show ? (
-          <div className="menu">
-            <div className="menu-header">
-              <div className="menu-header-right">
-                <img src={logo} alt="Code Academy" />
-              </div>
-              <div className="menu-header-left">
-                <CloseOutlined onClick={this.reverseShow} />
-              </div>
-            </div>
-            <div className="menu-list">
-              <NavLink
-                to="/"
-                exact
-                activeClassName="header-list-active"
-                className="header-list"
-              >
-                Home
-              </NavLink>
-              <NavLink
-                to="/search"
-                activeClassName="header-list-active"
-                className="header-list"
-              >
-                Search
-              </NavLink>
-              <NavLink
-                to="/Cohort"
-                activeClassName="header-list-active"
-                className="header-list"
-              >
-                Cohort
-              </NavLink>
-              <NavLink
-                to="/Alumni"
-                activeClassName="header-list-active"
-                className="header-list"
-              >
-                Alumni
-              </NavLink>
-              <NavLink
-                to="/internal-projects-overview"
-                activeClassName="header-list-active"
-                className="header-list"
-              >
-                Internal Projects
-              </NavLink>
-              <NavLink
-                to="/clients-projects-overview"
-                activeClassName="header-list-active"
-                className="header-list"
-              >
-                Remotely Project
-              </NavLink>
-            </div>
-            <div className="menu-social">
-              <span>FACEBOOK</span>
-              <span>INSTAGRAM</span>
-              <span>TWITTER</span>
-            </div>
+      <div>
+        <div className="main-header">
+          <div className="header-left">
+            <img src={logo} alt="Code Academy" />
           </div>
-        ) : null}
+          <div className="header-right">
+            <SearchOutlined />
+            <MenuOutlined onClick={this.reverseShow} />
+          </div>
+          {show ? (
+            <div>
+              <div className="menu">
+                <div className="menu-header">
+                  <div className="menu-header-right">
+                    <img src={logo} alt="Code Academy" />
+                  </div>
+                  <div className="menu-header-left">
+                    <CloseOutlined onClick={this.reverseShow} />
+                  </div>
+                </div>
+                <div className="menu-list">
+                  <NavLink
+                    to="/"
+                    exact
+                    activeClassName="header-list-active"
+                    className="header-list"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to="/search"
+                    activeClassName="header-list-active"
+                    className="header-list"
+                  >
+                    Search
+                  </NavLink>
+                  <NavLink
+                    to="/Cohort"
+                    activeClassName="header-list-active"
+                    className="header-list"
+                  >
+                    Cohort
+                  </NavLink>
+                  <NavLink
+                    to="/Alumni"
+                    activeClassName="header-list-active"
+                    className="header-list"
+                  >
+                    Alumni
+                  </NavLink>
+                  <NavLink
+                    to="/internal-projects-overview"
+                    activeClassName="header-list-active"
+                    className="header-list"
+                  >
+                    Internal Projects
+                  </NavLink>
+                  <NavLink
+                    to="/clients-projects-overview"
+                    activeClassName="header-list-active"
+                    className="header-list"
+                  >
+                    Remotely Project
+                  </NavLink>
+                </div>
+                <div className="menu-social">
+                  <span>FACEBOOK</span>
+                  <span>INSTAGRAM</span>
+                  <span>TWITTER</span>
+                </div>
+              </div>
+              <div className="header-overlay" />
+            </div>
+          ) : null}
+        </div>
       </div>
     );
   }
