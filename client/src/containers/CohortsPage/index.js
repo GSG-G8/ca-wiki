c7f3b94ca2ff62d2e25c687447c1ecbdd157f005
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-03 03:57:19 +0300

    Add slider and fix card slightly

--- client/src/containers/CohortsPage/index.js
+++ client/src/containers/CohortsPage/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { notification, Empty, Card } from 'antd';
+import ItemsCarousel from 'react-items-carousel';
 
 import './styles.css';
-
-const { Meta } = Card;
 
 class Cohorts extends Component {
   state = {
     data: [],
+    activeItemIndex: 0,
   };
 
   async componentDidMount() {
@@ -25,24 +25,46 @@ class Cohorts extends Component {
   }
 
   render() {
-    const { data } = this.state;
+    const { data, activeItemIndex } = this.state;
 
     return (
       <div>
         {data.length === 0 ? (
           <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="empty" />
         ) : (
-          <div>
-            {data.map((x) => (
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt={x.name} src={x.img_url} />}
-                key={x.id}
-              >
-                <Meta title={x.name} description={x.description} />
-              </Card>
-            ))}
+          <div style={{ padding: 0, maxWidth: '100%', margin: '0' }}>
+            <ItemsCarousel
+              infiniteLoop={false}
+              gutter={12}
+              activePosition="center"
+              chevronWidth={60}
+              disableSwipe={false}
+              alwaysShowChevrons={false}
+              numberOfCards={5}
+              slidesToScroll={3}
+              outsideChevron={false}
+              showSlither={false}
+              firstAndLastGutter
+              activeItemIndex={activeItemIndex}
+              requestToChangeActive={(value) =>
+                this.setState({ activeItemIndex: value })
+              }
+              rightChevron={'>'}
+              leftChevron={'<'}
+            >
+              {data.map((x) => (
+                <Card hoverable className="cohorts_card">
+                  <img
+                    alt={x.name}
+                    src={x.img_url}
+                    style={{ width: 120, height: 120 }}
+                    key={x.id}
+                  />
+                  <h2>{x.name}</h2>
+                  <p>{x.description}</p>
+                </Card>
+              ))}
+            </ItemsCarousel>
           </div>
         )}
       </div>
