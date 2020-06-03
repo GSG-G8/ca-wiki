8631c37cf196a31df99cdd1061897c7ab36a21a9
rehabas <reremo45@gmail.com>
2020-06-03 18:20:54 +0300

    query the Google Analytics Reporting API relates #111

---
+++ client/src/components/Charts/queryReport.js
@@ -0,0 +1,50 @@
+import { format } from 'date-fns';
+
+const queryReport = (props) => {
+  const { startDate, endDate, metrics, dimensions, orderBy, filter } = props;
+
+  const requestDimensions = (dimension) => {
+    const result = [];
+    dimension.forEach((item) => {
+      result.push({
+        name: item,
+      });
+    });
+    return result;
+  };
+  return window.gapi.client.request({
+    path: '/v4/reports:batchGet',
+    root: 'https://analyticsreporting.googleapis.com/',
+    method: 'POST',
+    body: {
+      reportRequests: [
+        {
+          viewId: process.env.REACT_APP_VIEW_ID,
+          filtersExpression: filter,
+          dateRanges: [
+            {
+              startDate: format(new Date(startDate), 'yyyy-MM-dd'),
+              endDate: format(new Date(endDate), 'yyyy-MM-dd'),
+            },
+          ],
+          metrics: [
+            {
+              expression: metrics,
+            },
+          ],
+          dimensions: requestDimensions(dimensions),
+          orderBys: orderBy
+            ? [
+                {
+                  fieldName: orderBy.fieldName,
+                  sortOrder: orderBy.order,
+                },
+              ]
+            : [],
+        },
+      ],
+    },
+  });
+};
+
+export default queryReport;
