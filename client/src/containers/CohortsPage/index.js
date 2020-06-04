aea57bde443e2c7863f5f81fc6943e83640848e2
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-04 05:13:30 +0300

    Style cohorts page

--- client/src/containers/CohortsPage/index.js
+++ client/src/containers/CohortsPage/index.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { notification, Empty, Card } from 'antd';
 import ItemsCarousel from 'react-items-carousel';
+import logo from '../../assets/images/logo.png';
+import UserContainer from '../../components/UserContainer';
 
 import './styles.css';
+import leftSvg from '../../assets/images/Group 2423.svg';
 
 class Cohorts extends Component {
   state = {
@@ -28,46 +31,57 @@ class Cohorts extends Component {
     const { data, activeItemIndex } = this.state;
 
     return (
-      <div>
-        {data.length === 0 ? (
-          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="empty" />
-        ) : (
-          <div style={{ padding: '0 60px', margin: '0 20px' }}>
-            <ItemsCarousel
-              infiniteLoop={false}
-              gutter={0}
-              activePosition="center"
-              chevronWidth={20}
-              disableSwipe={false}
-              alwaysShowChevrons={false}
-              numberOfCards={5}
-              slidesToScroll={1}
-              outsideChevron
-              showSlither={false}
-              firstAndLastGutter={false}
-              activeItemIndex={activeItemIndex}
-              requestToChangeActive={(value) =>
-                this.setState({ activeItemIndex: value })
-              }
-              rightChevron={'>'}
-              leftChevron={'<'}
-            >
-              {data.map((x) => (
-                <Card hoverable className="cohort_card">
-                  <img
-                    alt={x.name}
-                    src={x.img_url}
-                    className="cohort_img"
-                    key={x.id}
-                  />
-                  <h2>{x.name}</h2>
-                  <p>{x.description}</p>
-                </Card>
-              ))}
-            </ItemsCarousel>
-          </div>
-        )}
-      </div>
+      <UserContainer
+        rightPageColor="black"
+        headerLogo={logo}
+        isCohortPages={false}
+      >
+        <div className="container">
+          {' '}
+          <img src={leftSvg} alt="background" />
+        </div>
+        <div className="child_container">
+          <h1 className="title_heading">Cohorts</h1>
+          {data.length === 0 ? (
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="empty" />
+          ) : (
+            <div style={{ padding: '0 80px', margin: '0 auto'}}>
+              <ItemsCarousel
+                infiniteLoop={false}
+                gutter={12}
+                activePosition="center"
+                chevronWidth={60}
+                disableSwipe={false}
+                alwaysShowChevrons={false}
+                numberOfCards={5}
+                slidesToScroll={1}
+                outsideChevron
+                showSlither={false}
+                firstAndLastGutter={false}
+                activeItemIndex={activeItemIndex}
+                requestToChangeActive={(value) =>
+                  this.setState({ activeItemIndex: value })
+                }
+                rightChevron={'>'}
+                leftChevron={'<'}
+              >
+                {data.map((x) => (
+                  <Card hoverable className="cohort_card">
+                    <img
+                      alt={x.name}
+                      src={x.img_url}
+                      className="card_img"
+                      key={x.id}
+                    />
+                    <h2 className="card_heading">{x.name}</h2>
+                    <p className="card_paragraph">{x.description}</p>
+                  </Card>
+                ))}
+              </ItemsCarousel>
+            </div>
+          )}
+        </div>
+      </UserContainer>
     );
   }
 }
