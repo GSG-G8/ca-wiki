e16968eb678c2110d327ae5f738e8c629e5cc8e8
rehabas <reremo45@gmail.com>
2020-06-05 15:03:18 +0300

    edit fake data relates #131

--- client/src/containers/InternalProjects/index.js
+++ client/src/containers/InternalProjects/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+// import React, { Component } from 'react';
+// import { Pagination, notification } from 'antd';
+// import axios from 'axios';
+// import logo from '../../assets/images/logo.png';
+// import UserContainer from '../../components/UserContainer';
+// import './style.css';
 
-function InternalProjects() {
-  return <div>internal</div>;
-}
+// class InternalProjects extends Component {
+//   state = {
+//     data: [],
+//     startPage: 0,
+//     endPage: 4,
+//     total: 5,
+//   };
 
-export default InternalProjects;
+//   async componentDidMount() {
+//     try {
+//       const {
+//         location: { search },
+//       } = this.props;
+//       console.log('ddd', search)
+//       const res = await axios.get(`/api/v1/projects${search}`);
+//       const { data } = res.data;
+//       const total = data.length * 2.5;
+//       this.setState({ data, total });
+//     } catch (err) {
+//       const {
+//         response: {
+//           data: { message },
+//         },
+//       } = err;
+//       notification.error({
+//         message: 'Error',
+//         description: message,
+//       });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <UserContainer headerLogo={logo}>
+//         <div className="projects-container">
+//           <h1>Internal Projects Phase</h1>
+//         </div>
+//       </UserContainer>
+//     );
+//   }
+// }
+
+// export default InternalProjects;
