9b371f083fcc2f7e332779a67e0569e70c3c9f83
Muhammad Abdulhadi <42492094+Mu7ammadAbed@users.noreply.github.com>
2020-06-07 18:19:37 +0300

    Update client/src/app/index.js
    Add project type for redirect
    
    Co-authored-by: Rana Obeid <49689787+ranasobeid95@users.noreply.github.com>

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -112,7 +112,7 @@ class App extends Component {
               path={ROUTES.SPECIFIC_COHORT_PAGE}
               render={(props) => (
                 <Redirect
-                  to={`/Cohorts/${props.match.params.cohortId}/Projects`}
+                  to={`/Cohorts/${props.match.params.cohortId}/Projects?type=remotly`}
                 />
               )}
             />
