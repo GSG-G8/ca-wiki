e6bf9884baa0a880627accc8b76834d9c7927c92
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-06-07 11:23:06 +0300

    add isRed props to check the background color relates #122

--- client/src/containers/InternalProjectsOverview/index.js
+++ client/src/containers/InternalProjectsOverview/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import UserContainer from '../../components/UserContainer';
 import OverviewComponent from '../../components/OverviewComponent';
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/login-logo.jpeg';
 import InternalProjectsImg from '../../assets/images/Group 3039.svg';
 
 const data = {
@@ -11,9 +11,10 @@ const data = {
     'The Code Academy Is Palestine’s First Full-Stack Coding Bootcamp With A Flagship Campus In Gaza And Second Campus Opening In The West Bank In November 2018. The Code Academy Is A Joint Project Of Mercy Corps/Gaza Sky Geeks And Founders & Coders International. We Train 16 Students Per Cohort In A Full-Time, Intensive Course For 8 Weeks With An Additional 16 Weeks Of Project-Based Learning With Real-World Clients To Jumpstart Your Professional Portfolio. The Objective Is To Graduate As Full-Stack Developers Who Can Deploy Production-Grade Software Online And Secure High-Quality Jobs With Companies Or Work As Freelance Developers.',
   buttonText: 'See Community Projects Phase',
   imageSource: InternalProjectsImg,
-  btnLink: '/Cohorts/:cohortId/Projects/:project',
+  btnLink: '/projects?type=internal',
   isLeftImg: true,
   isDark: true,
+  isRed: true,
 };
 
 const InternalProjectsOverview = () => {
@@ -26,6 +27,7 @@ const InternalProjectsOverview = () => {
     btnLink,
     isLeftImg,
     isDark,
+    isRed,
   } = data;
   return (
     <UserContainer
@@ -42,6 +44,7 @@ const InternalProjectsOverview = () => {
         btnLink={btnLink}
         isLeftImg={isLeftImg}
         isDark={isDark}
+        isRed={isRed}
       />
     </UserContainer>
   );
