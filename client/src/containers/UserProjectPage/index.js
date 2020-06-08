9d96811ce56e9f22c3a2c00df9e2f893c5cf8e4f
rehabas <reremo45@gmail.com>
2020-06-08 16:18:17 +0300

    handle projects page relates #132

--- client/src/containers/UserProjectPage/index.js
+++ client/src/containers/UserProjectPage/index.js
@@ -21,11 +21,16 @@ class UserProject extends Component {
       const {
         location: { search },
       } = this.props;
-      const res = await axios.get(`/api/v1/projects${search}`);
-      const { data } = res.data;
-      const total = Math.ceil(data.length / 6) * 10;
-      this.setState({ data, total });
+      if (search) {
+        const res = await axios.get(`/api/v1/projects${search}`);
+        const { data } = res.data;
+        const total = Math.ceil(data.length / 6) * 10;
+        this.setState({ data, total });
+      }
     } catch (err) {
+      const {
+        history: { push },
+      } = this.props;
       const {
         response: {
           data: { message },
@@ -35,6 +40,7 @@ class UserProject extends Component {
         message: 'Error',
         description: message,
       });
+      push('/projects');
     }
   }
 
@@ -50,44 +56,69 @@ class UserProject extends Component {
     return (
       <UserContainer headerLogo={logo} isProjectsPage toolsTreeImg>
         <div className="projects-container">
-          {projectType === 'internal' ? (
-            <h1>Internal Projects Phase</h1>
+          {search ? (
+            <>
+              {projectType === 'internal' ? (
+                <h1>Internal Projects Phase</h1>
+              ) : (
+                <h1>Clients Projects Phase</h1>
+              )}
+              {data.length === 0 ? (
+                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="empty" />
+              ) : (
+                dataList.map((item) => (
+                  <>
+                    <Link to={`/projects/${item.id}`}>
+                      <div
+                        className="project"
+                        style={{
+                          background: `url(${item.img_url}) center center / cover no-repeat`,
+                        }}
+                      >
+                        <div className="project-name">
+                          <h2>{item.name}</h2>
+                        </div>
+                      </div>
+                    </Link>
+                    <Pagination
+                      className="pagination"
+                      current={currentPage}
+                      defaultCurrent={1}
+                      showQuickJumper
+                      onChange={(page) => {
+                        this.setState({
+                          currentPage: page,
+                          startPage: page * 6 - 6,
+                          endPage: page * 6,
+                        });
+                      }}
+                      total={total}
+                    />
+                  </>
+                ))
+              )}
+            </>
           ) : (
-            <h1>Clients Projects Phase</h1>
-          )}
-          {data.length === 0 ? (
-            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="empty" />
-          ) : (
-            dataList.map((item) => (
-              <>
-                <Link to={`/projects/${item.id}`}>
-                  <div
-                    className="project"
-                    style={{
-                      background: `url(${item.img_url}) center center / cover no-repeat`,
-                    }}
-                  >
-                    <div className="project-name">
-                      <h2>{item.name}</h2>
-                    </div>
-                  </div>
-                </Link>
-                <Pagination
-                  className="pagination"
-                  current={currentPage}
-                  defaultCurrent={1}
-                  showQuickJumper
-                  onChange={(page) => {
-                    this.setState({
-                      currentPage: page,
-                      startPage: page * 6 - 6,
-                      endPage: page * 6,
-                    });
-                  }}
-                  total={total}
-                />
-              </>
-            ))
+            <div className="project-page">
+              <div className="project-page-link">
+                <h2>Internal Projects</h2>
+                <a href="/projects?type=internal">
+                  <img
+                    src="https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80"
+                    alt="internal projects"
+                  />
+                </a>
+              </div>
+              <div className="project-page-link">
+                <h2>Remotely Projects</h2>
+                <a href="/projects?type=remotely">
+                  <img
+                    src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                    alt="remotely projects  "
+                  />
+                </a>
+              </div>
+            </div>
           )}
         </div>
       </UserContainer>
@@ -98,6 +129,7 @@ class UserProject extends Component {
 UserProject.propTypes = {
   location: PropTypes.func.isRequired,
   history: PropTypes.shape({
+    push: PropTypes.func,
     location: PropTypes.shape({
       search: PropTypes.string,
     }),
