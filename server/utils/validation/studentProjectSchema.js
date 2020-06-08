63360e126d4370dcd35dca22545aad0b0cf1a53a
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-09 01:37:06 +0300

    Handle the query to deal with values if they was 4,3,2 or 1

--- server/utils/validation/studentProjectSchema.js
+++ server/utils/validation/studentProjectSchema.js
@@ -1,8 +1,11 @@
 const yup = require('yup');
 
 const studentProjectSchema = yup.object({
-  studentId: yup.number().integer().positive().required(),
   projectId: yup.number().integer().positive().required(),
+  student1Id: yup.number().integer().positive().required(),
+  student2Id: yup.number().integer().positive(),
+  student3Id: yup.number().integer().positive(),
+  student4Id: yup.number().integer().positive(),
 });
 
 module.exports = studentProjectSchema;
