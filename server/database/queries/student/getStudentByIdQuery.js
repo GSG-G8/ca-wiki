13a3a42e4e5929fcfab326e6252ae7aad07474e5
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-08 17:53:36 +0300

    Undo some unnecessary changes

--- server/database/queries/student/getStudentByIdQuery.js
+++ server/database/queries/student/getStudentByIdQuery.js
@@ -2,4 +2,5 @@ const connection = require('../../connection');
 
 const getStudentByIdQuery = (id) =>
   connection.query('SELECT * FROM student WHERE id = $1', [id]);
+
 module.exports = getStudentByIdQuery;
